12f8bae7d850c75a3ad286b978ee827befe7fd7b
Wika <sastrawanwika@gmail.com>
2026-05-22 16:08:10 +0800

    fix service

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\SubscriptionController;
 
-Route::apiResource('users', UserController::class);
 Route::apiResource('services', ServiceController::class);
 Route::patch('services/{service}/activate', [
     ServiceController::class, 
